72b28f1a13fdd92c9db17cc7f7c00c1a48bd9763
alineflores <aline.flores@fatec.sp.gov.br>
2022-12-22 18:30:00 -0300

    commit

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.CONNECTIONSTRING)
-.then (() => {
-    console.log('BD conectado');
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
     app.emit('pronto');
-})
-.catch(e => console.log(e));
+  })
+  .catch(e => console.log(e));
 
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
@@ -19,22 +17,24 @@ const flash = require('connect-flash');
 const routes = require('./routes');
 const path = require('path');
 const meuMiddleware = require('./src/middlewares/middleware');
+const { async } = require('regenerator-runtime');
 
 app.use (express.urlencoded( {extended: true} ));
-
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
-    secret: 'abcdef',
+    secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
     store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
-    resave:false,
+    resave: false,
     saveUninitialized: false,
-    cookie:{
-        maxAge: 1000 * 60 * 60 * 24 * 7,
-        httpOnly: true
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true
     }
 });
+
 app.use(sessionOptions);
+
 app.use(flash());
 
 app.set('views' , path.resolve(__dirname, 'src' , 'views'));
--- src/models/HomeModel.js
+++ src/models/HomeModel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const HomeSchema = new mongoose.Schema({
-    titulo: {type: String, require: true}
+  titulo: { type: String, required: true },
+  descricao: String
 });
 
 const HomeModel = mongoose.model('Home', HomeSchema);
