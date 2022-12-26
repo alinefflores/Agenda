020ab2a57120e103f398d7c28b16474ae14134ea
alineflores <aline.flores@fatec.sp.gov.br>
2022-12-26 18:04:44 -0300

    commit

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -9,43 +8,54 @@ mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifi
     app.emit('pronto');
   })
   .catch(e => console.log(e));
-
+  
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
-
 const routes = require('./routes');
 const path = require('path');
-const meuMiddleware = require('./src/middlewares/middleware');
-const { async } = require('regenerator-runtime');
+const helmet = require('helmet');
+const csrf = require('csurf');
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
-app.use (express.urlencoded( {extended: true} ));
+app.use(helmet());
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
-    secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
-    store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-      httpOnly: true
-    }
+  secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true
+  }
 });
-
 app.use(sessionOptions);
-
 app.use(flash());
 
-app.set('views' , path.resolve(__dirname, 'src' , 'views'));
-app.set('view engine' , 'ejs');
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
 
-app.use(meuMiddleware);
+app.use(csrf());
+
+app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
 app.use(routes);
 
-app.on ('pronto' , () => {
-    app.listen(3000, () => {
-        console.log('Acessar http://localhost:3000');
-        console.log('servidor executando na porta 3000');
-    });
+app.on('pronto', () => {
+  app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+  });
 });
