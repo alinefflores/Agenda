a3ee701e537c4b84ce9861d12dd0c157076a8d43
alineflores <aline.flores@fatec.sp.gov.br>
2022-12-27 09:00:18 -0300

    commit

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -21,3 +21,12 @@ exports.middlewareGlobal = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
     next();
   };
+
+  exports.loginRequired = (req, res, next) => {
+    if(!req.session.user) {
+      req.flash('errors', 'Você precisa fazer login');
+      req.session.save(() => res.redirect('/'));
+    }
+
+    next();
+  };
